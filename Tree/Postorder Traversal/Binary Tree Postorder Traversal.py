8194f682cdd4c5af913837b39b71bb2ebfbfd656
kartik kurmi <ktkartik9@gmail.com>
2026-04-25 13:08:05 +0530

    Update Binary Tree Postorder Traversal.py

--- Tree/Postorder Traversal/Binary Tree Postorder Traversal.py
+++ Tree/Postorder Traversal/Binary Tree Postorder Traversal.py
@@ -3,9 +3,9 @@
 # Method: Recursive Depth-First Search 
 
 # Logic:
-      """Visit the nodes in the order: Left → Right → Root
+      """Visit the roots in the order: Left → Right → Root
       The helper function recursively explores the left subtree first
-      then the right subtree and finally appends the current nodes value to the result list"""
+      then the right subtree and finally appends the current roots value to the result list"""
 
 class Solution:
     def postorderTraversal(self, root):
